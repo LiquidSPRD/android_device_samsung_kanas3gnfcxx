bbdbc60500620c96856c5a3d02c6c2026d901f18
Y300-0100 <bosko.brajovich@gmail.com>
2016-05-09 17:35:45 +0200

    Liquify

---
+++ liquid.mk
@@ -0,0 +1,16 @@
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device_kanas3gnfcxx.mk)
+
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := kanas3gnfcxx
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kanas3gnfcxx
+PRODUCT_NAME := liquid_kanas3gnfcxx
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G355HN
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
